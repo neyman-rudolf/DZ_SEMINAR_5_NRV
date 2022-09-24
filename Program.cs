fe92403e48fdcdabe3923213b4ef67f6a8c43713
Rudolf Neyman <neyman.rudolf@yandex.ru>
2022-09-24 14:38:11 +0300

    Решение задачи №38

--- Program.cs
+++ Program.cs
@@ -25,23 +25,40 @@
 
 // [-4, -6, 89, 6] -> 0
 
-int[] array = new int[4]; 
-for (int i = 0; i < array.Length; i++)
-{
-     array[i] = new Random().Next(-99,100);
-}
-Console.WriteLine($"Array: [{String.Join("; ", array)}]");
-int sumNumbers = 0;
+// int[] array = new int[4]; 
+// for (int i = 0; i < array.Length; i++)
+// {
+//      array[i] = new Random().Next(-99,100);
+// }
+// Console.WriteLine($"Array: [{String.Join("; ", array)}]");
+// int sumNumbers = 0;
 
- for (int i = 0; i < array.Length; i++)
-  {
-     if (i%2==0) 
-     sumNumbers = sumNumbers + array[i];
-  }
+//  for (int i = 0; i < array.Length; i++)
+//   {
+//      if (i%2==0) 
+//      sumNumbers = sumNumbers + array[i];
+//   }
 
-Console.WriteLine($"Cумму элементов стоящих на нечётных позициях = {sumNumbers}");
+// Console.WriteLine($"Cумма элементов стоящих на нечётных позициях = {sumNumbers}");
 
 // Задача 38: Задайте массив вещественных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 
 // [3 7 22 2 78] -> 76
+
+int[] array = new int[4]; 
+for (int i = 0; i < array.Length; i++)
+{
+     array[i] = new Random().Next(0,100);
+}
+Console.WriteLine($"Array: [{String.Join("; ", array)}]");
+int min=array[0];
+int max=array[0];
+for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i]<=min) min = array[i];
+    if (array[i]>=max) max = array[i];
+  }
+Console.WriteLine($"Максимальный элемент = {max}" );
+Console.WriteLine($"Минимальный элемент= {min}" );
+Console.WriteLine($"Разница между максимальным и минимальным элементами = {max-min}" );
